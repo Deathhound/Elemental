b8ae96f3a2391767f805917bf8b6f73ee4ef5fac
Alexander <Alexandermastryukov@gmail.com>
2016-02-14 12:41:37 -0500

    Removed Android, CMake, Emscripten, IOs and MacOS project files

--- proj.android/install.bat
+++
@@ -1,2 +0,0 @@
-adb install -r bin/Elemental-debug.apk
-adb shell am start -n org.oxygine.Elemental/org.oxygine.Elemental.MainActivity
--- proj.android/install.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-adb install -r bin/Elemental-debug.apk
-adb shell am start -n org.oxygine.Elemental/org.oxygine.Elemental.MainActivity
